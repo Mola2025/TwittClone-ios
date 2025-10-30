a3d17d2cc4cf0941c279c8a77c2cacb2e7843898
David Molano <davidm0l4n0@gmail.com>
2025-10-30 16:40:53 -0400

    Initial Commit

---
+++ TwittClone/TwittCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwittCloneApp.swift
+//  TwittClone
+//
+//  Created by David Molano on 2025-10-30.
+//
+
+import SwiftUI
+
+@main
+struct TwittCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
